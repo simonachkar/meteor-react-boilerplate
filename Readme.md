e463a15fcf53168bd47e2d91c82d5d5a185da889
Simon Achkar <alachkarsimon@gmail.com>
2018-05-24 09:56:33 +0300

    Set a testing env with Mocha and Chai

--- Readme.md
+++ Readme.md
@@ -2,12 +2,17 @@
 
   A simple boilerplate containing an auth system, implemented with React and Meteor API.
   
+  
  
  ### Components 
- * Login
- * Singup
- * Dashboard
- * PrivateHeader
- * PrivateHeader
- 
-*routes implemented using React Routes*
+ Implementation of some basic components with a nice looking UI.
+  * Login
+  * Singup
+  * Dashboard
+  * PrivateHeader
+  * PrivateHeader
+  
+ Routing between components implemented using React Routes
+
+### Testing
+A complete test environment is set up, with Mocha and Chai. The libraries used are `cultofcoders:mocha` and `practicalmeteor:chai`. Some basic test cases including user validation are implemented in `users.test.js`. 
---
+++ imports/api/users.test.js
@@ -0,0 +1,32 @@
+import {Meteor} from 'meteor/meteor';
+import {validateNewUser} from './users';
+
+
+if (Meteor.isServer) {
+    describe('users', function () {
+
+        it('should allow valid email address', function() {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'simon@testing.com'
+                    }
+                ]
+            };
+            expect(validateNewUser(testUser)).to.be.true;
+        });
+
+        it('should not allow invalid email address', function() {
+            const testUser = {
+                emails: [
+                    {
+                        address: 'simon'
+                    }
+                ]
+            };
+            //const res = validateNewUser(testUser);
+            expect(() => {validateNewUser(testUser)}).to.throw();
+        });
+
+    });
+}
