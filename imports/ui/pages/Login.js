e1c8ccd7f3663db51a8c50c782d2606e864a6ddf
Simon Achkar <alachkarsimon@gmail.com>
2018-06-25 18:03:45 +0300

    Added a spinner to login and signup form

--- imports/ui/pages/Login.js
+++ imports/ui/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Meteor } from 'meteor/meteor';
+import { PulseLoader } from 'react-spinners';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -9,10 +10,16 @@ export default class Login extends React.Component {
     this.password = React.createRef();
     this.state = {
       error: '',
+      loading: false,
     };
   }
   onSubmit(e) {
     e.preventDefault();
+
+    this.setState({
+      error: '',
+      loading: true,
+    });
 
     const email = this.email.current.value.trim();
     const password = this.password.current.value.trim();
@@ -20,11 +27,13 @@ export default class Login extends React.Component {
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         this.setState({
-          error: 'Unable to login check email and password',
+          error: `Unable to login - ${err.reason}`,
+          loading: false,
         });
       } else {
         this.setState({
           error: '',
+          loading: false,
         });
       }
     });
@@ -35,6 +44,12 @@ export default class Login extends React.Component {
       <div className="boxed-view">
         <div className="boxed-view__box ">
           <h1>Login</h1>
+          <PulseLoader
+            color="#426cb4"
+            loading={this.state.loading}
+            size={10}
+            margin="5px"
+          />
           {this.state.error ? <p className="error">{this.state.error}</p> : undefined}
           <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form">
             <input type="email" ref={this.email} name="email" placeholder="Email" />
--- imports/ui/pages/Signup.js
+++ imports/ui/pages/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Accounts } from 'meteor/accounts-base';
+import { PulseLoader } from 'react-spinners';
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -9,10 +10,16 @@ export default class Signup extends React.Component {
     this.password = React.createRef();
     this.state = {
       error: '',
+      loading: false,
     };
   }
   onSubmit(e) {
     e.preventDefault();
+
+    this.setState({
+      error: '',
+      loading: true,
+    });
 
     const email = this.email.current.value.trim();
     const password = this.password.current.value.trim();
@@ -20,6 +27,7 @@ export default class Signup extends React.Component {
     if (password.length < 9) {
       return this.setState({
         error: 'Password must be more than 9 characters long',
+        loading: false,
       }); // using return to stop function execution
     }
 
@@ -27,10 +35,12 @@ export default class Signup extends React.Component {
       if (err) {
         this.setState({
           error: err.reason,
+          loading: false,
         });
       } else {
         this.setState({
           error: '',
+          loading: false,
         });
       }
     });
@@ -40,6 +50,13 @@ export default class Signup extends React.Component {
       <div className="boxed-view">
         <div className="boxed-view__box">
           <h1>Join</h1>
+          <PulseLoader
+            color="#426cb4"
+            loading={this.state.loading}
+            size={10}
+            margin="5px"
+            padding="5px"
+          />
           {this.state.error ? <p className="error">{this.state.error}</p> : undefined}
           <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form">
             <input type="email" ref={this.email} name="email" placeholder="Email" />
